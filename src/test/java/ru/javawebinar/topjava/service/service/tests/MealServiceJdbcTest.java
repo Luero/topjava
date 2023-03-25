f712eb64993d80979bbb5b263b756b28778d5977
Ksu Er <kseniapodguzova@gmail.com>
2023-03-25 13:44:28 +0300

    HW5 - 3-4

---
+++ src/test/java/ru/javawebinar/topjava/service/service/tests/MealServiceJdbcTest.java
@@ -0,0 +1,9 @@
+package ru.javawebinar.topjava.service.service.tests;
+
+import org.springframework.test.context.ActiveProfiles;
+import ru.javawebinar.topjava.ActiveDbProfileResolver;
+import ru.javawebinar.topjava.service.MealServiceTestBase;
+
+@ActiveProfiles(resolver = ActiveDbProfileResolver.class, profiles = "jdbc")
+public class MealServiceJdbcTest extends MealServiceTestBase {
+}
