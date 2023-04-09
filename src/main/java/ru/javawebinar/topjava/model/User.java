9f2983b644d8726a5dfa1c8972981b5cbd536930
Ksu Er <kseniapodguzova@gmail.com>
2023-04-09 21:27:15 +0300

    HW6 - optional to 2.4

--- src/main/java/ru/javawebinar/topjava/model/User.java
+++ src/main/java/ru/javawebinar/topjava/model/User.java
@@ -83,7 +83,8 @@ public class User extends AbstractNamedEntity {
         this(id, name, email, password, DEFAULT_CALORIES_PER_DAY, true, new Date(), List.of(roles));
     }
 
-    public User(Integer id, String name, String email, String password, int caloriesPerDay, boolean enabled, Date registered, Collection<Role> roles) {
+    public User(Integer id, String name, String email, String password, int caloriesPerDay, boolean enabled, Date registered,
+                Collection<Role> roles) {
         super(id, name);
         this.email = email;
         this.password = password;
--- src/main/java/ru/javawebinar/topjava/repository/jdbc/JdbcUserRepository.java
+++ src/main/java/ru/javawebinar/topjava/repository/jdbc/JdbcUserRepository.java
@@ -2,6 +2,7 @@ package ru.javawebinar.topjava.repository.jdbc;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.support.DataAccessUtils;
+import org.springframework.jdbc.core.BatchPreparedStatementSetter;
 import org.springframework.jdbc.core.BeanPropertyRowMapper;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.namedparam.BeanPropertySqlParameterSource;
@@ -9,10 +10,15 @@ import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
 import org.springframework.jdbc.core.simple.SimpleJdbcInsert;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
+import ru.javawebinar.topjava.model.Role;
 import ru.javawebinar.topjava.model.User;
 import ru.javawebinar.topjava.repository.UserRepository;
 
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Set;
 
 @Repository
 @Transactional(readOnly = true)
@@ -37,41 +43,96 @@ public class JdbcUserRepository implements UserRepository {
     }
 
     @Override
+    @Transactional
     public User save(User user) {
+        //       ValidationUtil.validate(user);
         BeanPropertySqlParameterSource parameterSource = new BeanPropertySqlParameterSource(user);
-
         if (user.isNew()) {
             Number newKey = insertUser.executeAndReturnKey(parameterSource);
             user.setId(newKey.intValue());
-        } else if (namedParameterJdbcTemplate.update(
-                "UPDATE users SET name=:name, email=:email, password=:password, " +
-                        "registered=:registered, enabled=:enabled, calories_per_day=:caloriesPerDay WHERE id=:id",
+            setRoles(user.id(), user.getRoles());
+        } else if (namedParameterJdbcTemplate.update("UPDATE users SET name=:name, email=:email, " +
+                        "password=:password, registered=:registered, enabled=:enabled, " +
+                        "calories_per_day=:caloriesPerDay WHERE id=:id",
                 parameterSource) == 0) {
             return null;
+        } else {
+            updateRoles(user.id(), user.getRoles());
         }
         return user;
     }
 
     @Override
+    @Transactional
     public boolean delete(int id) {
         return jdbcTemplate.update("DELETE FROM users WHERE id=?", id) != 0;
     }
 
     @Override
     public User get(int id) {
-        List<User> users = jdbcTemplate.query("SELECT * FROM users WHERE id=?", ROW_MAPPER, id);
-        return DataAccessUtils.singleResult(users);
+        User user = DataAccessUtils.singleResult(jdbcTemplate.query("SELECT * FROM users WHERE id=?", ROW_MAPPER, id));
+        if (user != null) {
+            setRoles(user);
+        }
+        return user;
     }
 
     @Override
     public User getByEmail(String email) {
 //        return jdbcTemplate.queryForObject("SELECT * FROM users WHERE email=?", ROW_MAPPER, email);
-        List<User> users = jdbcTemplate.query("SELECT * FROM users WHERE email=?", ROW_MAPPER, email);
-        return DataAccessUtils.singleResult(users);
+        User user = DataAccessUtils.singleResult(jdbcTemplate.query("SELECT * FROM users WHERE email=?", ROW_MAPPER, email));
+        if (user != null) {
+            setRoles(user);
+        }
+        return user;
     }
 
     @Override
     public List<User> getAll() {
-        return jdbcTemplate.query("SELECT * FROM users ORDER BY name, email", ROW_MAPPER);
+        return jdbcTemplate.query("SELECT u.id, u.name, u.email, u.password, u.registered, u.enabled, u.calories_per_day," +
+                "string_agg(ur.role, ', ')  AS roles FROM users u LEFT JOIN user_role ur ON u.id = ur.user_id GROUP BY " +
+                "u.id ORDER BY u.name, u.email", ROW_MAPPER);
+    }
+
+    private void setRoles(User user) {
+        user.setRoles(jdbcTemplate.query("SELECT * FROM user_role WHERE user_id = ?", rs -> {
+            List<Role> roles = new ArrayList<>();
+            while (rs.next()) {
+                roles.add(Role.valueOf(rs.getString("role")));
+            }
+            return roles;
+        }, user.getId()));
+    }
+
+    private int[] setRoles(int userId, Set<Role> roles) {
+        List<Role> rolesList = new ArrayList<>(roles);
+        return jdbcTemplate.batchUpdate("INSERT INTO user_role (user_id, role) values (?, ?)", new BatchPreparedStatementSetter() {
+            @Override
+            public void setValues(PreparedStatement ps, int i) throws SQLException {
+                ps.setInt(1, userId);
+                ps.setString(2, rolesList.get(i).toString());
+            }
+
+            @Override
+            public int getBatchSize() {
+                return rolesList.size();
+            }
+        });
+    }
+
+    public int[] updateRoles(int userId, Set<Role> roles) {
+        List<Role> rolesList = new ArrayList<>(roles);
+        return jdbcTemplate.batchUpdate("UPDATE user_role SET role=? WHERE user_id=?", new BatchPreparedStatementSetter() {
+            @Override
+            public void setValues(PreparedStatement ps, int i) throws SQLException {
+                ps.setString(1, rolesList.get(i).toString());
+                ps.setInt(2, userId);
+            }
+
+            @Override
+            public int getBatchSize() {
+                return rolesList.size();
+            }
+        });
     }
 }
